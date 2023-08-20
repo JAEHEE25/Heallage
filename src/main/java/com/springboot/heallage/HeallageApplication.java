c5f42c4c62e4c735ed08ccd4ea3ff62ebf57d717
JAEHEEE <y2hjjh@naver.com>
2023-08-21 00:42:04 +0900

    Initial commit

---
+++ src/main/java/com/springboot/heallage/HeallageApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.heallage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeallageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeallageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/heallage/HeallageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.heallage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeallageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
