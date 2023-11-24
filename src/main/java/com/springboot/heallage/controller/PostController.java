496eef82d5296e474f2d5e7b0c9aa3602ac77fd5
jaehee <y2hjjh@naver.com>
2023-11-24 21:23:43 +0900

    Chore: Swagger config 파일 추가

--- src/main/java/com/springboot/heallage/controller/PostController.java
+++ src/main/java/com/springboot/heallage/controller/PostController.java
@@ -5,14 +5,13 @@ import com.springboot.heallage.data.dto.BaseResponse;
 import com.springboot.heallage.data.dto.post.PostRequestDto;
 import com.springboot.heallage.sevice.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/post")
+@RequestMapping(value = "api/v1/post")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
